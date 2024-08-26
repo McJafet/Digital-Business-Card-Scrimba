d606f806bf3782917720d099181252af5a6a520f
McJafet <jafet2195@gmail.com>
2024-08-25 23:03:29 -0500

    fixing import GitHub-Icon.png

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import './Footer.css';
 import twitter from '../../images/Twitter-Icon.png';
 import facebook from '../../images/Facebook-Icon.png';
 import instagram from '../../images/Instagram-Icon.png';
-import github from '../../images/Github-Icon.png';
+import github from '../../images/GitHub-Icon.png';
 
 
 export default function Footer() {
